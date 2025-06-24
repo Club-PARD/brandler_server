06936d2372bac365d1fe58e10b6daf60b788fc75
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2025-06-24 11:13:30 +0900

    Initial first commit

---
+++ src/main/java/com/pard/server/fashion_muse/FashionMuseApplication.java
@@ -0,0 +1,13 @@
+package com.pard.server.fashion_muse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionMuseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionMuseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pard/server/fashion_muse/FashionMuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.server.fashion_muse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionMuseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
