3c05b4950fa70b6a50450eaa576fd12e2238eca3
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2025-07-03 20:18:53 +0900

    [bakc] feat : swagger upload

---
+++ src/main/java/com/pard/server/fashion_muse/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.pard.server.fashion_muse.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("brandler")
+                .description("brandler 스웨거")
+                .version("1.0.0");
+    }
+
+    //http://localhost:8080/swagger-ui/index.html
+
+}
